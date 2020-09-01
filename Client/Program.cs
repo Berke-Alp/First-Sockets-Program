c101395da7def68ed86083461e420a118aa6fcf1
Alp Çalışkan <berkealp@yandex.com>
2020-09-01 09:56:54 +0300

    First push

---
+++ Client/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+	class Program
+	{
+		static int retryCount = 1;
+
+		static void Main(string[] args)
+		{
+			Console.Title = "Sockets Client";
+			Console.Clear();
+
+			// Bir client tanımlıyoruz
+			TcpClient tcpc = new TcpClient();
+
+			// Bağlanana kadar 5 kez dene
+			while(retryCount != 6)
+			{
+				try
+				{
+					// Tanımlanan client ile localhost'un 8085 portuna bağlanıyoruz
+					Console.WriteLine("Trying to connect to 127.0.0.1:8085");
+					tcpc.Connect("127.0.0.1", 8085);
+				}
+				catch (SocketException) { }
+
+				if (tcpc.Connected) break;
+
+				if (retryCount == 5) break;
+				else if (retryCount == 4) Console.WriteLine($"[{retryCount}] Retrying to connect to localhost one last time...");
+				else
+				{
+					Console.WriteLine($"[{retryCount}] Can't connect to localhost, retrying in 2s...");
+					Thread.Sleep(2000);
+				}
+				retryCount++;
+			}
+
+			// Eğer 5 kez yeniden denenmiş ise programı sonlandır
+			if(retryCount == 6)
+			{
+				Console.WriteLine("Couldn't connect to localhost.\nPress any key to exit the program...");
+				Console.ReadKey();
+				return;
+			}
+
+			Console.WriteLine("Connected to localhost...");
+			
+			try
+			{
+				Console.Write("Enter your message: ");
+				int sentbytes = tcpc.Client.Send(Encoding.UTF8.GetBytes(Console.ReadLine()));
+				tcpc.Close();
+				Console.WriteLine($"-> You sent {sentbytes} bytes to the server. If you want to send another message, press Y.");
+				if (Console.ReadKey(false).Key == ConsoleKey.Y) Main(args);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Hata: " + ex.Message);
+			}
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Sockets";
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+			Console.WriteLine("Listening on port 8085...");
+
+			TcpListener listener = new TcpListener(new IPAddress(0), 8085);
+
+			while (true)
+			{
+				try
+				{
+					// Dinleyici dinlemeye başlar
+					listener.Start();
+					// Gelen veriyi göndereni client olarak tanımlıyoruz
+					TcpClient client = listener.AcceptTcpClient();
+
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.Write("Incoming connection: ");
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.WriteLine(client.Client.RemoteEndPoint.ToString());
+
+					// Gelen veri NetworkStream olduğu için bu Stream'i okuyoruz
+					StreamReader reader = new StreamReader(client.GetStream());
+					// Gelen veriyi tümüyle ekrana basıyoruz
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.WriteLine(reader.ReadToEnd());
+					Console.WriteLine();
+
+					// Okuyucuyu durduruyoruz
+					reader.Close();
+					// Dinlemeyi bırakıyoruz
+					listener.Stop();
+				}
+				catch (Exception ex)
+				{
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine("Hata oluştu: " + ex.Message);
+					break;
+				} 
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
